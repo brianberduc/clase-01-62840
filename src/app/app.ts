a260ab004ad52411e4ef7bac6e2683c7701ebb9c
Brian Berduc <brian.berduc@gmail.com>
2026-05-13 09:30:35 -0300

    Haciendo la tarea del break

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,8 +4,10 @@ import { Component, signal } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('clase-01-62840-clasico');
+  userName = 'Brian Berduc';
+  userAge = 30;
 }
